d3aa70aec4608988d09009ccfb3bcbfb1fbffe8a
rjtechcomputer0314-commits <rjtechcomputer0314@gmail.com>
2026-05-18 06:40:04 -1100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const esperarHeader = setInterval(()=>{
+
+        const menuToggle =
+        document.getElementById("menu-toggle");
+
+        const navMenu =
+        document.getElementById("nav-menu");
+
+        if(menuToggle && navMenu){
+
+            clearInterval(esperarHeader);
+
+            menuToggle.addEventListener("click", ()=>{
+
+                navMenu.classList.toggle("active");
+
+            });
+
+        }
+
+    },100);
+
+});
+
+/* =========================================
+  BOTÓN QUE ESTÁ EL WATSAP Y EL TELÉFONO
+========================================= */
+
+document.addEventListener("click",(e)=>{
+
+    const mainBtn =
+    e.target.closest("#mainFloatBtn");
+
+    if(mainBtn){
+
+        const floatItems =
+        document.getElementById("floatItems");
+
+        floatItems.classList.toggle("active");
+    }
+
+});
+
+
+/* =========================================
+   PARA LAS PREGUNTAS DEL INDEX QUE ESTA
+========================================= */
+
+const preguntas =
+document.querySelectorAll(".pregunta-item");
+
+preguntas.forEach((item)=>{
+
+    const titulo =
+    item.querySelector(".pregunta-titulo");
+
+    titulo.addEventListener("click",()=>{
+
+        item.classList.toggle("active");
+
+    });
+
+});
