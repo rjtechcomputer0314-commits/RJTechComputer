e110e03d9914687c2c749f52ed630fab3411e565
rjtechcomputer0314-commits <rjtechcomputer0314@gmail.com>
2026-06-08 03:50:24 -0500

    Update visitas.js

--- js/visitas.js
+++ js/visitas.js
@@ -1,17 +1,44 @@
 /* para las  visitas*/
-document.addEventListener("DOMContentLoaded", async ()=>{
+document.addEventListener(
+    "DOMContentLoaded",
+    contarVisita
+);
+
+async function contarVisita(){
 
     const { data, error } =
     await supabaseClient
     .from("visitas")
-    .insert([
-        {
-            total: 1
-        }
-    ])
+    .select("*")
+    .eq("id",1)
+    .single();
+
+    console.log("LECTURA:", data);
+    console.log("ERROR LECTURA:", error);
+
+    if(error) return;
+
+    const nuevoTotal =
+    Number(data.total) + 1;
+
+    console.log("NUEVO TOTAL:", nuevoTotal);
+
+    const { data:updateData,
+            error:updateError } =
+    await supabaseClient
+    .from("visitas")
+    .update({
+        total:nuevoTotal
+    })
+    .eq("id",1)
     .select();
 
-    console.log(data);
-    console.log(error);
+    console.log("UPDATE DATA:", updateData);
+    console.log("UPDATE ERROR:", updateError);
 
-});
+    document
+    .getElementById("contadorVisitas")
+    .textContent =
+    nuevoTotal;
+
+}
